89d11e9f83fc0f6aacedc4b52c5b03f7ef440b59
Caio lima <caiohenrys50@gmail.com>
2023-06-25 02:04:32 -0300

    implementando modals

--- phrases/script.js
+++ phrases/script.js
@@ -1,73 +1,118 @@
 const form = document.getElementById("form");
 const buttonRegister = document.getElementById("buttonRegister");
 const phrasesTextarea = document.getElementById("phrase");
-const phrasesStayHere = document.querySelector(".phrasesStayHere")
-const borderColorPhrases = document.querySelector(".border-color-phrases")
+const phrasesStayHere = document.querySelector(".phrasesStayHere");
 
 buttonRegister.addEventListener("click", function () {
   disableButton();
 });
 
 function enableButton() {
-  // buttonRegister.disabled = false;
-  buttonRegister.style.cursor = "pointer";
+  buttonRegister.disabled = true;
+  buttonRegister.style.cursor = 'not-allowed';
+  buttonRegister.style.backgroundColor = '#5b5f6e';
 }
 
 function disableButton() {
-  // buttonRegister.disabled = true;
-  buttonRegister.style.cursor = "not-allowed";
+  buttonRegister.disabled = false;
+  buttonRegister.style.cursor = 'pointer';
+  buttonRegister.style.backgroundColor = '#5540cd';
 }
 
-phrasesTextarea.addEventListener("keyup",  () => {
-  if (phrasesTextarea.value) {
+phrasesTextarea.addEventListener("keyup", () => {
+  if (!phrasesTextarea.value) {
     enableButton();
   } else {
     disableButton();
   }
 });
 
-
 phrasesTextarea.addEventListener('input', () => {
   phrasesTextarea.value = phrasesTextarea.value
-  ? phrasesTextarea.value.trimStart()
-  : '';
+    ? phrasesTextarea.value.trimStart()
+    : '';
 });
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const inputChecked = document.querySelector('input[name="prioridade"]:checked')
-  const textarea = document.getElementById("phrase")
+  const inputChecked = document.querySelector('input[name="prioridade"]:checked');
+  const textarea = document.getElementById("phrase");
+
   try {
     if (!inputChecked || !textarea.value) {
-      // error
-      alert('por favor digitar todos os campos')
-    }
-    if (inputChecked) {
-      const priority = inputChecked.value;
-      getPriorityColor(priority);
+ 
+      Swal.fire({
+        title: 'Por favor, preencha todos os campos.',
+        showCancelButton: false,
+        confirmButtonText: 'OK',
+        customClass: {
+          confirmButton: 'my-custom-button-class',
+          title: 'my-custom-title-class'
+        }
+      });
+      return;
     }
 
-  createPhrase({
+    const isPhraseDuplicate = checkDuplicatePhrase(textarea.value);
+    if (isPhraseDuplicate) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        title: 'Frase já foi criada!',
+        icon: 'warning'
+      })
+      return;
+    }
+
+    const successAlert = displaySuccessAlert();
+    createPhrase({
       phrase: textarea.value,
-      priority: document.querySelector('input[name="prioridade"]:checked').value
-    })
+      priority: inputChecked.value,
+      successAlert: successAlert
+    });
 
-    textarea.value = ''
-    inputChecked.checked = false
+    existingPhrases.push(textarea.value);
 
-    } catch (error) {
-      console.log('error', error)
-    }
+    textarea.value = '';
+    inputChecked.checked = false;
+  } catch (error) {
+    console.log('error', error);
+  }
+});
 
-  function getPriorityColor(priority) {
-    if(priority === "low"){
-      console.log(priority)
+function displaySuccessAlert() {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
-    if(priority === "medium"){
-      console.log(priority)
-    }
-    if(priority === "high"){
-      console.log(priority)
-    }}
-  });
+  })
+  
+  Toast.fire({
+    title: 'Frase criada com sucesso',
+  })
+}
+
+const existingPhrases = [];
+
+function checkDuplicatePhrase(phrase) {
+  const isDuplicate = existingPhrases.includes(phrase);
+  return isDuplicate;
+}
+
+
